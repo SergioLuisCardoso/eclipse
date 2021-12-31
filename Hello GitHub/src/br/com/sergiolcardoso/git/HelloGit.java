8b2b5e707c26bcc5a609a4e6f47c04a4a690182c
Sergio L Cardoso <55567543+SergioLuisCardoso@users.noreply.github.com>
2021-12-30 21:35:53 -0300

    Added files from upload

---
+++ Hello GitHub/src/br/com/sergiolcardoso/git/HelloGit.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package br.com.sergiolcardoso.git;
+
+/**
+ * @author Sergio Luis Cardoso
+ * Date: 30/12/2021
+ * Title: Integração do Eclipse com o GitHub
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Git");
+
+	}
+
+}
